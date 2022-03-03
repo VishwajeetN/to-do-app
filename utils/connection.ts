50a793efe075253ad981482bc92f40c480178594
VishwajeetN <100856443+VishwajeetN@users.noreply.github.com>
2022-03-04 00:00:58 +0530

    Add files via upload

---
+++ utils/connection.ts
@@ -0,0 +1,25 @@
+//IMPORT MONGOOSE
+import mongoose, { Model } from "mongoose"
+
+// CONNECTING TO MONGOOSE (Get Database Url from .env.local)
+const { DATABASE_URL } = process.env
+
+// connection function
+export const connect = async () => {
+  const conn = await mongoose
+    .connect(DATABASE_URL as string)
+    .catch(err => console.log(err))
+  console.log("Mongoose Connection Established")
+
+  // OUR TODO SCHEMA
+  const TodoSchema = new mongoose.Schema({
+    //_id:String, //added
+    item: String,
+    completed: Boolean,
+  })
+
+  // OUR TODO MODEL
+  const Todo = mongoose.models.Todo || mongoose.model("Todo", TodoSchema)
+
+  return { conn, Todo }
+}
